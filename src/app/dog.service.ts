17ca629f80458fa1917e6e43e5f6e3b36b446435
James Carlin <james.carlin@infor.com>
2019-10-24 12:22:24 +0200

    third commit

--- src/app/dog.service.ts
+++ src/app/dog.service.ts
@@ -1,15 +1,51 @@
 import { Injectable } from '@angular/core';
 import { Dog } from './dog';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DogService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  getDogs(): void {
+  private dogsUrl = 'api/dogs';
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content Type': 'application/json'})
+  };
+
+  private  handleError<T> (operation = 'operation', result?: T){
+    return (error: any): Observable<T> => {
+
+      // TODO: improve error message
+      console.log('Error');
+
+      //Keep app running
+      return of(result as T);
+    };
+  }
+
+  getDogs(): Observable<Dog[]> {
+    return this.http.get<Dog[]>(this.dogsUrl).pipe(
+      tap(_ => console.log('fetched dogs')),
+      catchError(this.handleError<Dog[]>('getDogs', []))
+    );
+  }
+
+  likeDog() {
+
+  }
+
+  removeDog() {
+
+  }
+
+  nameDog() {
 
   }
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Dog } from './dog';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InMemoryDataService {
 
-  constructor() { }
+  createDbDogs() {
+    const dogs = [
+      {}
+    ];
+    return {dogs};
+  }
+
+  createDbFavorites() {
+    const faves = [
+      {}
+    ];
+    return {faves};
+  }
+
 }
